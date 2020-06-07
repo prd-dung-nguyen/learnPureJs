96c9ae631525a556c3e26d9f94b50b3af74378eb
junng <dung.nguyen@paradox.ai>
2020-06-08 00:13:21 +0700

    Second commit - Done lesson 4

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ var app = new Vue ({
     el: "#app",
     data: {
         product: 'Socks',
-        img: './materials/greenSocks.jpg'
+        img: './materials/greenSocks.jpg',
+        inStock: false,
+        details: ["80% Cotton", "20% Poly", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'Green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'Blue'
+            }
+        ]
     }
 });
